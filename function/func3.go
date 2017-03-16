92ae07697eedf0bb5b40aad0d0bd9881881ba172
liuruichao <liuruichao555@126.com>
2017-03-16 11:38:04 +0800

    change err

--- function/func3.go
+++ function/func3.go
@@ -6,11 +6,15 @@ import (
 )
 
 func add(a int, b int) (int, error) {
-	return a + b, errors.New("计算错误")
+	if a < 0 || b < 0 {
+		return 0, errors.New("a和b不能小于0")
+	}
+	return a + b, nil
 }
 
 func main() {
-	result, err := add(1, 3)
+	//result, err := add(1, 3) // error
+	result, err := add(-3, 1)  // success
 	if err != nil {
 		fmt.Println(err)
 		return
