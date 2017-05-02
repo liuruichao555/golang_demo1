89a80244ff6d86798ed12a527b0e5b24880166b0
liuruichao <liuruichao555@126.com>
2017-05-02 15:30:17 +0800

    study map

--- make/test2.go
+++ make/test2.go
@@ -1,10 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 type Person struct {
-	name string
-	age  int
+	name       string
+	age        int
+	createTime int64
 }
 
 func main() {
@@ -16,4 +20,14 @@ func main() {
 
 	var _slice2 []int = []int{1, 2}
 	fmt.Println(_slice2)
+
+	person := Person{name: "liuruichao", age: 20, createTime: time.Now().Unix()}
+	fmt.Println(person)
+
+	var person2 map[string]Person = make(map[string]Person)
+	person2["1"] = person
+
+	fmt.Println(person2)
+
+	fmt.Println(person2["1"])
 }
