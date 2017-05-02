e69355dc221aa603ca0b5248e24c94bae9fac2cb
liuruichao <liuruichao555@126.com>
2017-05-02 15:20:28 +0800

    study make

--- make/test1.go
+++ make/test1.go
@@ -1,1 +1,27 @@
-package make
+package main
+
+import (
+	"fmt"
+	google_protobuf1 "github.com/golang/protobuf/ptypes/timestamp"
+)
+
+type ChaincodeMessage struct {
+	Timestamp *google_protobuf1.Timestamp `protobuf:"bytes,2,opt,name=timestamp" json:"timestamp,omitempty"`
+	Payload   []byte                      `protobuf:"bytes,3,opt,name=payload,proto3" json:"payload,omitempty"`
+	Txid      string                      `protobuf:"bytes,4,opt,name=txid" json:"txid,omitempty"`
+	//Proposal  *SignedProposal             `protobuf:"bytes,5,opt,name=proposal" json:"proposal,omitempty"`
+}
+
+func main() {
+	var str chan string
+	fmt.Println("no make: ", str)
+
+	str = make(chan string, 10)
+	fmt.Println("make no value: ", str)
+
+	str <- "one"
+	fmt.Println("value one: ", str)
+
+	<- str
+	fmt.Println("out value: ", str)
+}
--- make/test2.go
+++ make/test2.go
@@ -1,1 +1,19 @@
 package main
+
+import "fmt"
+
+type Person struct {
+	name string
+	age  int
+}
+
+func main() {
+	var _slice []int = make([]int, 5, 10)
+	fmt.Println(_slice)
+
+	var _slice1 []int = make([]int, 5)
+	fmt.Println(_slice1)
+
+	var _slice2 []int = []int{1, 2}
+	fmt.Println(_slice2)
+}
