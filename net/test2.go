21942ad71752d15862f19f833f5b1fa7ffc88e1b
liuruichao <liuruichao555@126.com>
2017-05-03 14:47:47 +0800

    study net

---
+++ net/test2.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"net/http"
+	"fmt"
+	"io/ioutil"
+)
+
+func main() {
+	client := &http.Client{}
+	url := "http://www.baidu.com"
+	request, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	response, err := client.Do(request)
+	if err != nil {
+		panic(err)
+	}
+	if response.StatusCode == 200 {
+		html, err := ioutil.ReadAll(response.Body)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(string(html))
+	} else {
+		fmt.Println("访问失败")
+	}
+}
