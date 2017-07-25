81770ed5abd32a42b255423be268526971e96122
liuruichao <liuruichao555@126.com>
2017-07-25 13:03:33 +0800

    add map

---
+++ strings/maptest1.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	userMap := make(map[string]string)
+	userMap["name"] = "liuruichao"
+	userMap["age"] = "20"
+	for k, v := range userMap {
+		fmt.Printf("key: %s, value: %s.\n", k, v)
+	}
+}
